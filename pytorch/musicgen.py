d7a0e6adb48201bb9adc2fae30078b3ec0d818b3
Louis Arge <louislva@gmail.com>
2023-09-08 23:36:57 +0000

    Remove conditioning in PyTorch too

--- pytorch/musicgen.py
+++ pytorch/musicgen.py
@@ -1,7 +1,6 @@
 import torch
 from torch import nn
 from transformer import Transformer
-from condition_fuser import ConditionFuser
 from huggingface_hub import hf_hub_download
 
 class MusicGen(nn.Module):
@@ -12,13 +11,7 @@ class MusicGen(nn.Module):
         self.codebook_size = codebook_size
         self.dim = dim
         self.depth = depth
-        
-        # self.condition_provider = lm.condition_provider
-        self.fuser = ConditionFuser(
-            fuse2cond={'sum': [], 'cross': ['description'], 'prepend': [], 'input_interpolate': []},
-            cross_attention_pos_emb=False,
-            cross_attention_pos_emb_scale=1
-        )
+
         self.emb = nn.ModuleList([nn.Embedding(codebook_size + 1, dim) for _ in range(codebook_count)])
         self.transformer = Transformer(dim=dim, depth=24)
         self.out_norm = nn.LayerNorm(dim)
@@ -26,11 +19,7 @@ class MusicGen(nn.Module):
 
     def forward(self, x):
         x = sum([emb(x[:,i,:]) for i, emb in enumerate(self.emb)])
-        # the zeros had a ?batch_sze+?? of 2 not 1 in meta's code, so need to look into what that was
-        x, z = self.fuser(x, {
-            "description": (torch.zeros(2, 1, 1024).to(x.device), torch.zeros(2, 1).to(x.device)),
-        })
-        x = self.transformer(x, cross_attention_src=z)
+        x = self.transformer(x, cross_attention_src=torch.zeros((2, 1, 1024)).to(x.device))
         x = self.out_norm(x)
         x = torch.stack([linear(x) for linear in self.linears], dim=1)
         return x
